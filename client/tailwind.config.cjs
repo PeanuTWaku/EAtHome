e267a6124f857122923b46719ea9cd2beb5d9652
alan910127 <alan469357@gmail.com>
2022-11-09 03:02:27 +0800

    :lipstick: Add inter var font

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "rgb(139 92 246)",
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
