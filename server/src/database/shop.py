063e7c49cac002935c5c4fe2bf4fe7d68d0bfd6b
alan910127 <alan469357@gmail.com>
2022-11-10 03:14:21 +0800

    :sparkles: Add shop models and schemas

---
+++ server/src/database/shop.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field, Relationship, SQLModel
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class ShopRead(SQLModel):
+    """Represent the response model for a shop."""
+
+    name: str
+    category: str
+    latitude: float
+    longitude: float
+    revenue: int
+
+    owner_name: str
+
+
+class ShopCreate(SQLModel):
+    """Represent the input fields for creating a shop."""
+
+    name: str
+    category: str
+    latitude: float = Field(ge=-90, le=90)
+    longitude: float = Field(ge=-180, le=180)
+
+    owner_name: str
+
+
+class Shop(SQLModel, table=True):
+    """Represent the fields of a shop stored in database."""
+
+    name: str = Field(primary_key=True)
+    category: str = Field(index=True)
+    latitude: float = Field(ge=-90, le=90)
+    longitude: float = Field(ge=-180, le=180)
+    revenue: int = Field(default=0, ge=0)
+
+    owner_name: str
+    owner: User = Relationship(back_populates="shopname")
+
+
+class ShopUpdate(SQLModel):
+    """Represent the fields of a shop that can be updated through api routes."""
+
+    category: str | None = None
+    latitude: float | None = Field(default=None, ge=-90, le=90)
+    longitude: float | None = Field(default=None, ge=-180, le=180)
