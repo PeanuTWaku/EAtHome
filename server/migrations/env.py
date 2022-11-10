7ee4860669b71fd8eb0f4e55650571142cef2785
alan910127 <alan469357@gmail.com>
2022-11-11 00:32:37 +0800

    :bank: Initial migration

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -11,6 +11,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlmodel import SQLModel  # isort:skip
 
 """import sql models here"""
+from src.database import Order, Shop, User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
