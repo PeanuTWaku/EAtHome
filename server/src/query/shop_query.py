735dbf02ad17b2cca7970506e9a1f1e017ba93f2
alan910127 <alan469357@gmail.com>
2022-11-11 17:50:09 +0800

    :sparkles: Add distance filter in shop query

--- server/src/query/shop_query.py
+++ server/src/query/shop_query.py
@@ -1,8 +1,11 @@
+from sqlalchemy import func
 from sqlmodel import select
 
 from ..database import AsyncSession
 from ..database.shop import Shop, ShopCreate, ShopUpdate
 from .base_query import BaseQuery
+
+EARTH_RADIUS = 6371
 
 
 class ShopQuery(BaseQuery[Shop, ShopCreate, ShopUpdate]):
@@ -18,13 +21,39 @@ class ShopQuery(BaseQuery[Shop, ShopCreate, ShopUpdate]):
         limit: int = 100,
     ) -> list[Shop]:
 
+        distance_stmt = (
+            2
+            * EARTH_RADIUS
+            * func.asin(
+                func.sqrt(
+                    0.5
+                    - func.cos(func.radians(latitude - Shop.latitude)) / 2
+                    + func.cos(func.radians(Shop.latitude))
+                    * func.cos(func.radians(latitude))
+                    * (1 - func.cos(func.radians(longitude - Shop.longitude)))
+                    / 2
+                )
+            )
+        )
+
         if category is not None:
             stmt = (
                 select(Shop)
-                .where(Shop.category == category)
+                .where(
+                    Shop.category == category,
+                    distance_stmt <= distance,
+                )
                 .offset(offset)
                 .limit(limit)
             )
+        else:
+            stmt = (
+                select(Shop)
+                .where(distance_stmt <= distance)
+                .offset(offset)
+                .limit(limit)
+            )
+
         result = await session.execute(stmt)
         return result.scalars().all()
 
