f5bd6acbb32cbc257cadbf34b8f32136504e21be
BaBa0525 <hsunhsun0525@gmail.com>
2022-11-11 17:24:27 +0800

    :construction: Modify shop_query function

--- server/src/query/shop_query.py
+++ server/src/query/shop_query.py
@@ -6,17 +6,27 @@ from .base_query import BaseQuery
 
 
 class ShopQuery(BaseQuery[Shop, ShopCreate, ShopUpdate]):
-    async def get_by_category(
-        self, session: AsyncSession, category: str, offset: int = 0, limit: int = 100
+    async def get_by_filters(
+        self,
+        session: AsyncSession,
+        *,
+        category: str | None = None,
+        latitude: float,
+        longitude: float,
+        distance: int,
+        offset: int = 0,
+        limit: int = 100,
     ) -> list[Shop]:
-        stmt = select(Shop).where(Shop.category == category).offset(offset).limit(limit)
+
+        if category is not None:
+            stmt = (
+                select(Shop)
+                .where(Shop.category == category)
+                .offset(offset)
+                .limit(limit)
+            )
         result = await session.execute(stmt)
         return result.scalars().all()
-
-    async def get_by_distance(
-        self, session: AsyncSession, distance: int, offset: int = 0, limit: int = 100
-    ) -> list[Shop]:
-        ...
 
 
 shop = ShopQuery(Shop)
